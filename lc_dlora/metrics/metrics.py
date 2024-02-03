49ae41f1d810c74adec23c4a4129b772795cd182
yeoshuheng <yeoshuheng@gmail.com>
2024-02-03 22:01:33 +0800

    feat: Add accuracy for binary

--- lc_dlora/metrics/metrics.py
+++ lc_dlora/metrics/metrics.py
@@ -11,3 +11,16 @@ def accuracy(model, evaluation_set):
     acc = no_correct / no_seen
     model.train()
     return acc
+
+def accuracy_binary(model, evaluation_set):
+    model.eval()
+    no_correct, no_seen = 0, 0
+    with torch.no_grad():
+        for input, label in evaluation_set:
+            output = torch.sigmoid(model(input))
+            output = torch.where(output > 0.5, 1, 0)
+            no_seen += label.size(0)
+            no_correct += (output == label).sum().item()
+    acc = no_correct / no_seen
+    model.train()
+    return acc
--- lc_dlora/utils.py
+++ lc_dlora/utils.py
@@ -10,7 +10,7 @@ def merge_base_lora(alpha : torch.Tensor, beta : torch.Tensor,
    return torch.add(base, scaling * torch.matmul(alpha, beta))
 
 def flatten_weight_tensor(tensor_list):
-    flattened = np.concatenate([tensor.detach().clone().flatten().numpy() for tensor in tensor_list])
+    flattened = np.concatenate([tensor.flatten().numpy() for tensor in tensor_list])
     return flattened
 
 def check_and_create_dir(fp):
@@ -36,7 +36,7 @@ class SyntheticData(Dataset):
     def __getitem__(self, idx):
         return self.data[idx], self.labels[idx]
     
-def generate_synthetic_dataloader(mu_1, mu_2, std_1, std_2, test_size=10000, t_size=100000):
+def generate_synthetic_dataloader(mu_1, mu_2, std_1, std_2, test_size=1000, t_size=10000):
     dataset_1 = torch.normal(mean=mu_1, std=std_1, size=(t_size, 2), dtype=torch.float32)
     dataset_1_labels = torch.full(size=(t_size,1), fill_value=0, dtype=torch.float32)
     dataset_2 = torch.normal(mean=mu_2, std=std_2, size=(t_size, 2), dtype=torch.float32)
