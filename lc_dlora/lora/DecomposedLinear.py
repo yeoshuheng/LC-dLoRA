fix: Fix model not learning due todetached gradients

lc_dlora/lora/DecomposedLinear.py
@@ -11,7 +11,7 @@ class DecomposedLinear(nn.Module):
                              dtype = torch.float32, requires_grad = True)
         self.alpha = nn.Parameter(alpha_t, requires_grad = True)
         self.beta = nn.Parameter(beta_t, requires_grad = True)
-        self.bias = nn.Parameter(bias.clone(), requires_grad = True)
+        self.bias = nn.Parameter(bias, requires_grad = True)
         torch.nn.init.kaiming_uniform_(self.alpha, 
                                        a = math.sqrt(5))
         torch.nn.init.zeros_(self.beta)

lc_dlora/utils.py
@@ -36,15 +36,19 @@ class SyntheticData(Dataset):
     def __getitem__(self, idx):
         return self.data[idx], self.labels[idx]
     
-def generate_synthetic_dataloader(mu_1, mu_2, std_1, std_2):
+def generate_synthetic_dataloader(mu_1, mu_2, std_1, std_2, test_size=1000):
     dataset_1 = torch.normal(mean=mu_1, std=std_1, size=(10000, 2), dtype=torch.float32)
     dataset_1_labels = torch.full(size=(10000,1), fill_value=0, dtype=torch.float32)
     dataset_2 = torch.normal(mean=mu_2, std=std_2, size=(10000, 2), dtype=torch.float32)
     dataset_2_labels = torch.full(size=(10000,1), fill_value=1, dtype=torch.float32)
     full_data = torch.cat((dataset_1, dataset_2))
     full_label = torch.cat((dataset_1_labels, dataset_2_labels))
-    dataset_full = SyntheticData(full_data[:-100, :].clone(), full_label[:-100].clone())
-    dataset_validation = SyntheticData(full_data[-100:,:].clone(), full_label[-100:].clone())
+    full_zipped = list(zip(full_data, full_label))
+    np.random.shuffle(full_zipped) # Shuffle before split
+    full_data = torch.stack([x[0] for x in full_zipped])
+    full_label = torch.stack([x[1] for x in full_zipped])
+    dataset_full = SyntheticData(full_data[:-test_size, :], full_label[:-test_size])
+    dataset_validation = SyntheticData(full_data[-test_size:,:], full_label[-test_size:])
     print("Full data : {} | Validation data : {}".format(len(dataset_full), len(dataset_validation)))
     train_ = DataLoader(dataset_full, batch_size=32, shuffle=True, drop_last=True)
     valid_ = DataLoader(dataset_validation, batch_size=32, shuffle=True, drop_last=True)